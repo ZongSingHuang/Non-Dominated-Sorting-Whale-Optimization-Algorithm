907a1459caa5a1056a48cceaabee8ed1da35195c
Matthew Huang <a26833765@gmail.com>
2023-02-21 00:39:39 +0800

    feat: 擁擠度

--- NSWOA.py
+++ NSWOA.py
@@ -1,5 +1,8 @@
-import numpy as np
 import functools
+import itertools
+from operator import attrgetter, itemgetter
+
+import numpy as np
 
 
 class NSWOA:
@@ -23,15 +26,94 @@ class NSWOA:
 
     def opt(self):
         # 初始化候選解
-        X = self.initial_population()
+        population = [self.initial_chromosome() for _ in range(self.size_pop)]
 
         # 計算適應值
-        F = self.calc_benchmark(X)
+        population = self.calc_benchmark(population=population)
 
         # 非支配快速排序
+        population = self.fast_nondominated_sort(population=population)
 
-    def initial_population(self):
-        X = np.random.uniform(
-            low=self.lb, high=self.ub, size=[self.size_pop, self.size_dim]
-        )
-        return X
+        # 擁擠度
+        population = self.crowding_distance(population=population)
+
+    # 初始化染色體
+    def initial_chromosome(self) -> dict:
+        chromosome = {
+            "X": np.random.uniform(low=self.lb, high=self.ub, size=[self.size_dim]),
+            "F": np.zeros(self.size_obj),
+            "輸給幾組": 0,
+            "贏了誰": [],
+        }
+        return chromosome
+
+    # 快速非支配排序
+    def fast_nondominated_sort(self, population) -> list:
+        FRONTs = [[]]
+        for master_idx, master in enumerate(population):
+            master["擁擠度"] = 0
+            master["輸給幾組"] = 0
+            master["贏了誰"] = []
+            for slave_idx, slave in enumerate(population):
+                if self.dominates(master, slave):
+                    master["贏了誰"].append(slave_idx)
+                elif self.dominates(slave, master):
+                    master["輸給幾組"] += 1
+                else:
+                    pass
+
+            if master["輸給幾組"] == 0:
+                master["推薦等級"] = 0
+                FRONTs[0].append(master_idx)
+
+        i = 0
+        while len(FRONTs[i]):
+            FRONT = []
+            for master_idx in FRONTs[i]:
+                master = population[master_idx]
+                for slave_idx in master["贏了誰"]:
+                    slave = population[slave_idx]
+                    slave["輸給幾組"] -= 1
+                    if slave["輸給幾組"] == 0:
+                        slave["推薦等級"] = i + 1
+                        FRONT.append(slave_idx)
+            i += 1
+            FRONTs.append(FRONT)
+
+        population.sort(key=itemgetter("推薦等級"))
+        return population
+
+    # 支配判定
+    def dominates(self, p1: dict, p2: dict) -> bool:
+        and_condition = True
+        or_condition = False
+
+        # 望小
+        for i in range(self.size_obj):
+            and_condition = and_condition and p1["F"][i] <= p2["F"][i]
+            or_condition = or_condition or p1["F"][i] < p2["F"][i]
+        return and_condition and or_condition
+
+    # 擁擠度
+    def crowding_distance(self, population: list) -> list:
+        # 分群
+        FRONTs = itertools.groupby(population, key=itemgetter("推薦等級"))
+
+        # by 推薦等級
+        for i, FRONT in FRONTs:
+            FRONT = list(FRONT)
+            FRONT_len = len(FRONT)
+            # by 目標式個數
+            for j in range(self.size_obj):
+                FRONT.sort(key=lambda chromosome: chromosome["F"][j])
+                FRONT[0]["擁擠度"] = np.inf
+                FRONT[-1]["擁擠度"] = np.inf
+                F_all = [t["F"][j] for t in FRONT]
+                scale = max(F_all) - min(F_all)
+                if scale == 0:
+                    scale = 1
+                for k in range(1, FRONT_len - 1):
+                    FRONT[k]["擁擠度"] += (
+                        FRONT[k + 1]["F"][j] - FRONT[k - 1]["F"][j]
+                    ) / scale
+        return population
--- benchmark.py
+++ benchmark.py
@@ -1,2 +1,13 @@
-def ztd3(X):
-    return X
+import numpy as np
+
+
+def ztd3(population: list) -> list:
+    for idx, chromosome in enumerate(population):
+        X = chromosome["X"]
+        F = np.zeros(2)
+        n = len(X)
+        g = 1 + 9 * np.sum(X[1:]) / (n - 1)
+        F[0] = X[0]
+        F[1] = 1 - np.sqrt(X[0] / g) - (X[0] / g) * np.sin(10 * np.pi * X[0])
+        population[idx]["F"] = F
+    return population
